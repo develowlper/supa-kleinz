dc73946c8d1dc1a23ca1a9f71d6b6da3d3ca129c
Daniel Schulz <schulz@sensape.com>
2021-12-19 01:06:37 +0100

    fix: spacing on pages

--- pages/index.js
+++ pages/index.js
@@ -4,50 +4,48 @@ export default function Home() {
   return (
     <>
       <Head title="kleinz" />
-      <div className="container mx-auto py-2">
-        <div>
-          <h1 className="text-3xl">Welcome to Kleinz</h1>
-          <h2>Roadmap:</h2>
-          <ul>v1: Q2/22</ul>
-          <h2>Release Notes:</h2>
-          <h3>
-            v0.3:
-            <ul>
-              <li>
-                Features:
-                <ul>
-                  <li>First version of genitals (gnihihihihih)</li>
-                  <li>A bit more overhead than before</li>
-                </ul>
-              </li>
-            </ul>
-          </h3>
-          <h3>
-            v0.2:
-            <ul>
-              <li>
-                Features:
-                <ul>
-                  <li>Started development of hands and feet</li>
-                  <li>A bit more overhead than before</li>
-                </ul>
-              </li>
-            </ul>
-          </h3>
-          <h3>
-            v0.1:
-            <ul>
-              <li>
-                Features:
-                <ul>
-                  <li>Initial build</li>
-                  <li>Stable foundation for upcoming features</li>
-                  <li>A bit of overhead</li>
-                </ul>
-              </li>
-            </ul>
-          </h3>
-        </div>
+      <div>
+        <h1 className="text-3xl">Welcome to Kleinz</h1>
+        <h2>Roadmap:</h2>
+        <ul>v1: Q2/22</ul>
+        <h2>Release Notes:</h2>
+        <h3>
+          v0.3:
+          <ul>
+            <li>
+              Features:
+              <ul>
+                <li>First version of genitals (gnihihihihih)</li>
+                <li>A bit more overhead than before</li>
+              </ul>
+            </li>
+          </ul>
+        </h3>
+        <h3>
+          v0.2:
+          <ul>
+            <li>
+              Features:
+              <ul>
+                <li>Started development of hands and feet</li>
+                <li>A bit more overhead than before</li>
+              </ul>
+            </li>
+          </ul>
+        </h3>
+        <h3>
+          v0.1:
+          <ul>
+            <li>
+              Features:
+              <ul>
+                <li>Initial build</li>
+                <li>Stable foundation for upcoming features</li>
+                <li>A bit of overhead</li>
+              </ul>
+            </li>
+          </ul>
+        </h3>
       </div>
     </>
   );
