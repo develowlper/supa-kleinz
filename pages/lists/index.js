a287f59d25643a08bbd8c0f6080d99bed27ce73a
Daniel Schulz <schulz@sensape.com>
2021-12-22 03:09:57 +0100

    fix: remived lists

--- pages/lists/index.js
+++ pages/lists/index.js
@@ -1,17 +1,11 @@
 import Head from 'components/Head';
-import { NavLink } from 'components/Navigation';
+import { NavLink } from 'components/NavLink';
 
 export default function Lists() {
   return (
     <div>
       <Head title="lists" />
-      <div>
-        <h2>Lists:</h2>
-        <div className="flex gap-2">
-          <NavLink href="/lists/todo" label="Todo" />
-          <NavLink href="/lists/tobuy" label="Tobuy" />
-        </div>
-      </div>
+      <div></div>
     </div>
   );
 }
