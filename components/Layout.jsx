20a21e22b4bdca3475f0b97ad3308922483e940e
Daniel Schulz <schulz@sensape.com>
2022-02-24 01:11:07 +0100

    fix: missing loading bar on page transition

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,16 +1,30 @@
-import { useRouter } from 'next/router';
+import Router, { useRouter } from 'next/router';
+import { useState } from 'react';
 import { Navbar, Footer } from './Navigation';
 import Toolbar from './Toolbar';
 
 export default function Layout({ children, ignore = ['/signin'] }) {
   const router = useRouter();
 
+  const [progress, setProgress] = useState(false);
+
+  Router.events.on('routeChangeStart', () => {
+    setProgress(true);
+    //function will fired when route change started
+  });
+
+  Router.events.on('routeChangeComplete', () => {
+    setProgress(false);
+    //function will fired when route change ended
+  });
+
   if (ignore.some((x) => x === router.pathname)) {
     return children;
   }
 
-  if (true) {
-    return (
+  return (
+    <>
+      {progress && <div className="h-1 bg-fuchsia-700 animate-pulse"></div>}
       <div className="flex flex-col h-screen">
         <Toolbar />
         <Navbar />
@@ -19,6 +33,6 @@ export default function Layout({ children, ignore = ['/signin'] }) {
         </main>
         <Footer />
       </div>
-    );
-  }
+    </>
+  );
 }
